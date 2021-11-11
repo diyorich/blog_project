01b2c61085a2f15df79111f040b987b6735c1ba3
Diyorich PC <netherdraker2001@gmail.com>
2021-11-12 01:34:55 +0500

    pushing local repo

---
+++ app/Tag.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use Sluggable;
+    public function posts()
+    {
+        return $this->belongsToMany(
+            Post::class,
+            'post_tags',
+            'tag_id',
+            'post_id'
+        );
+    }
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+            ];
+    }
+}
